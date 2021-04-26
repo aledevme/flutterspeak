705644013699a3cb0264adfa2e9a667ce8852d4d
Alejandro Gonzalez <alexgve7sv@outlook.com>
2021-04-26 13:03:26 +0200

    🔎 adding search field

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -17,7 +17,8 @@ class _HomeState extends State<Home> {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Header(),
-              helloMessage(name: 'Alejandro', message: 'Exploremos fotografias')
+              helloMessage(name: 'Alejandro', message: 'Exploremos fotografias'),
+              searchField()
             ],
           ),
         )
@@ -47,5 +48,49 @@ class _HomeState extends State<Home> {
     );
   }
 
-  
+  Widget searchField(){
+    return Container(
+      margin: EdgeInsets.symmetric(
+        vertical: 20,
+        horizontal: 30
+      ),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(8),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.2),
+            spreadRadius: 1,
+            blurRadius: 20,
+            offset: Offset(2,7), // changes position of shadow
+          ),
+        ]
+      ),
+      child: TextField(
+        decoration: InputDecoration(
+          contentPadding: EdgeInsets.symmetric(
+            horizontal: 20,
+            vertical: 18
+          ),
+
+          suffixIcon: Padding(
+            padding: EdgeInsets.only(
+              right: 20
+            ),
+            child: Icon(Icons.search, size: 30, color: Colors.orange),
+          ),
+          border: InputBorder.none,
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide.none,
+            borderRadius: BorderRadius.circular(10)
+          ),
+          hintStyle: TextStyle(
+            fontWeight: FontWeight.w700,
+            color: Colors.grey[400]
+          ),
+          hintText: 'Busca algo...'
+        ),
+      ),
+    );
+  }
 }
